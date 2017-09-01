a9516a3b260205efe20d173d9e624226ea6a80ff
sweetpea6 <sweetpea6@naver.com>
2017-09-01 14:24:20 +0900

    추가

--- artwork/js_src/_artwork_exhibit_detail_page.js
+++ artwork/js_src/_artwork_exhibit_detail_page.js
@@ -1,3 +1,18 @@
 /**
  * Created by Administrator on 2017-08-31.
  */
+$(function(){
+  $('.top-button-img').on('click', function(e){
+
+    e.preventDefault();
+
+    var href = $(this).attr('href');
+
+    var scrollPosition =  $(href).offset().top ;
+
+    $('html, body').stop().animate({
+      scrollTop : scrollPosition
+    });
+
+  });
+});
--- artwork/js_src/_gnb.js
+++ artwork/js_src/_gnb.js
@@ -3,12 +3,8 @@
  */
 
 var e = document.getElementById('btn');
+
 e.addEventListener('click', function() {
   if (this.className == 'on') this.classList.remove('on');
   else this.classList.add('on');
-});
-
-$(function(){
-
-
 });
