7b332c06708d5b9d60a363543457702b53f68023
sweetpea6 <sweetpea6@naver.com>
2017-09-04 16:19:36 +0900

    추가

--- artwork/js_src/_artwork_artjob_detail_page.js
+++ artwork/js_src/_artwork_artjob_detail_page.js
@@ -1,3 +1,14 @@
 /**
  * Created by Administrator on 2017-08-29.
  */
+
+$(function(){
+  $(window).on("load",function(){
+
+    $('.detail-content').mCustomScrollbar({
+      autoHideScrollbar:true,
+      theme:"minimal-dark"
+    });
+
+  });
+});
