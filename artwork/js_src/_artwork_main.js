eb4be493e1ea0ca011a5afee8564aaa8877a8f3d
sweetpea6 <sweetpea6@naver.com>
2017-09-05 18:11:40 +0900

    no message

--- artwork/js_src/_artwork_main.js
+++ artwork/js_src/_artwork_main.js
@@ -4,6 +4,17 @@
 
 $(function(){
 
+    $('.down-btn').on('click', function(){
+
+      var scrollPosition =  $('.section2').offset().top ;
+
+      $('html, body').stop().animate({
+        scrollTop : scrollPosition
+      });
+
+    });
+
+
   $('.contents-list-item-mask').on('mouseenter', function(){
 
     $(this).children('.contents-list-item-mask-text').addClass('on');
